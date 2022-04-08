c44c243a522a23e23ecea7a466f328cd7c5c0629
Jared <38020231+Jared-Glenn@users.noreply.github.com>
2022-04-08 14:52:00 -0600

    Add files via upload

---
+++ Chore_Chart.py
@@ -0,0 +1,63 @@
+from datetime import date
+
+measure_from = date(2022, 4, 8)
+tday = date.today()
+diff = (tday - measure_from).days
+
+day_mod = None
+
+x = 0
+
+def chore(x):
+    if x == 0:
+        return """Feed the cat,
+Empty the Surprise box,"""
+    elif x == 1:
+        return """Clean the front room,
+Do an extra chore for Mom,"""
+    elif x == 2:
+        return """Unload the Dishwasher,
+Load the Dishwasher,"""
+    elif x == 3:
+        return """Wipe the bathrooms down,
+Clear and clean the table,"""
+
+
+inp = input("Whose chores do you want to know?")
+
+if inp == "Ethan":
+    day_mod = (diff + 0) % 4
+    chores = chore(day_mod)
+    print("""
+Today, Ethan needs to:
+"""+
+chores,"""
+Make his bed,
+And read for 30 minutes.""")
+elif inp == "Ellie" or inp == "Elloise":
+    day_mod = (diff + 1) % 4
+    chores = chore(day_mod)
+    print("""
+Today, Ellie needs to:
+"""+
+chores,"""
+Make her bed,
+And read for 30 minutes.""")
+elif inp == "Abbie" or inp == "Abigail":
+    day_mod = (diff + 2) % 4
+    chores = chore(day_mod)
+    print("""
+Today, Abbie needs to:
+"""+
+chores,"""
+Make her bed,
+And read for 30 minutes.""")
+elif inp == "Gennie" or inp == "Genevieve":
+    day_mod = (diff + 3) % 4
+    chores = chore(day_mod)
+    print("""
+Today, Gennie needs to:
+"""+
+chores,"""
+Make her bed,
+And read for 30 minutes.""")
